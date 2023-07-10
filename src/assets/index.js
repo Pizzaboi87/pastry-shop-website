ebc50211bcfeb2062c9faf1ae0b53c9cc051f687
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-07-10 16:40:12 +0300

    minor changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,4 +1,4 @@
-import logo from './logo-transparent.png';
+import logo from './logo.png';
 import macaron from './macaron.webp';
 import eclair from './eclair.webp';
 import cream from './cream.webp';
