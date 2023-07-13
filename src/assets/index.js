e872eaa90069e92ffd444cc985d0085803aeeaa9
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-07-13 14:58:48 +0300

    1 binaries uploaded

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,5 +4,6 @@ import eclair from './eclair.webp';
 import cream from './cream.webp';
 import torta from './torta.webp';
 import history from './history.webp';
+import waffle from './waffle.webp';
 
-export { logo, macaron, eclair, cream, torta, history };
+export { logo, macaron, eclair, cream, torta, history, waffle };
