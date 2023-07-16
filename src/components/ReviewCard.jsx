201a2eb17c9a9064f8fabcba831b3b15dfbf7c24
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-07-16 13:10:09 +0300

    added review cards

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,48 @@
+import { logo } from "../assets";
+import { Icon } from "@iconify/react";
+
+const ReviewCard = ({ review }) => {
+	const stars = review.stars;
+
+	return (
+		<div className="w-[20rem] h-[30rem] border relative flex flex-col items-center rounded-xl shadow-xl">
+			<img
+				src={review.background}
+				alt="background"
+				className="w-full h-[25%] bg-cover rounded-t-xl object-cover"
+			/>
+			<img
+				src={review.profile}
+				alt="profile"
+				className="bg-white rounded-full w-[10rem] h-[10rem] absolute top-[10%] object-cover border-4 border-white"
+			/>
+			<div className="flex flex-col items-center absolute top-[45%]">
+				<h1 className="text-text text-[1.6rem] font-[600]">{review.name}</h1>
+				<h2 className="text-text text-[1.3rem] font-[400]">{review.job}</h2>
+			</div>
+			<p className="italic text-text text-[1rem] font-[300] absolute top-[65%] text-justify px-4">
+				{`"${review.review}"`}
+			</p>
+			<span className="absolute top-[88%] flex w-full left-[33%]">
+				{[...Array(5)].map((_, index) => (
+					<Icon
+						key={index}
+						icon="ph:star-light"
+						className="text-yellowdark text-[1.4rem]"
+					/>
+				))}
+			</span>
+			<span className="absolute top-[88%] flex w-full left-[33%]">
+				{[...Array(stars)].map((_, index) => (
+					<Icon
+						key={index}
+						icon="ph:star-fill"
+						className="text-yellowdark text-[1.4rem]"
+					/>
+				))}
+			</span>
+		</div>
+	);
+};
+
+export default ReviewCard;
--- src/components/index.js
+++ src/components/index.js
@@ -8,22 +8,24 @@ import MainContent from "./MainContent";
 import Navbar from "./Navbar";
 import Paragraphs from "./Paragraphs";
 import RecipeCard from "./RecipeCard";
+import ReviewCard from "./ReviewCard";
 import ScrollToTop from "./ScrollToTop";
 import SearchForm from "./SearchForm";
 import TextAndImage from "./TextAndImage";
 
 export {
-  Awning,
-  CourseCard,
-  CourseForm,
-  Footer,
-  Image,
-  LinkButton,
-  MainContent,
-  Navbar,
-  Paragraphs,
-  RecipeCard,
-  ScrollToTop,
-  SearchForm,
-  TextAndImage,
+	Awning,
+	CourseCard,
+	CourseForm,
+	Footer,
+	Image,
+	LinkButton,
+	MainContent,
+	Navbar,
+	Paragraphs,
+	RecipeCard,
+	ReviewCard,
+	ScrollToTop,
+	SearchForm,
+	TextAndImage,
 };
