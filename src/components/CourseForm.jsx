747ae295edb58f9c6cb245e74098957afc9006c2
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-07-16 16:27:56 +0300

    minor changes

--- src/components/CourseForm.jsx
+++ src/components/CourseForm.jsx
@@ -7,178 +7,178 @@ import { phoneInputStyle } from "../styles";
 import "react-phone-input-2/lib/bootstrap.css";
 
 const CourseForm = ({ courses }) => {
-  const [loading, setLoading] = useState(false);
-  const [form, setForm] = useState({
-    name: "",
-    phone: "",
-    email: "",
-    course: "",
-    question: "",
-  });
+	const [loading, setLoading] = useState(false);
+	const [form, setForm] = useState({
+		name: "",
+		phone: "",
+		email: "",
+		course: "",
+		question: "",
+	});
 
-  const successSwal = () => {
-    Swal.fire({
-      icon: "success",
-      title: "Success!",
-      text: `Thank you for your interest in our ${form.course} course. We will contact you soon!`,
-    });
-  };
+	const successSwal = () => {
+		Swal.fire({
+			icon: "success",
+			title: "Success!",
+			text: `Thank you for your interest in our ${form.course} course. We will contact you soon!`,
+		});
+	};
 
-  const errorSwal = (error) => {
-    Swal.fire({
-      icon: "error",
-      title: "Something went wrong!",
-      text: error,
-    });
-  };
+	const errorSwal = (error) => {
+		Swal.fire({
+			icon: "error",
+			title: "Something went wrong!",
+			text: error,
+		});
+	};
 
-  const valueCheck = (form) => {
-    const nameRegex = /^[A-Za-z-/ñÑáÁéÉíÍóÓöÖőŐüÜűŰ\s]+$/;
-    const questionRegex = /^[A-Za-z0-9,.\-;:?!()%"@$/€ñÑáÁéÉíÍóÓöÖőŐüÜűŰ\s]+$/;
+	const valueCheck = (form) => {
+		const nameRegex = /^[A-Za-z-/ñÑáÁéÉíÍóÓöÖőŐüÜűŰ\s]+$/;
+		const questionRegex = /^[A-Za-z0-9,.\-;:?!()%"@$/€ñÑáÁéÉíÍóÓöÖőŐüÜűŰ\s]+$/;
 
-    if (!nameRegex.test(form.name)) {
-      errorSwal("Please enter a valid name.");
-      return;
-    }
+		if (!nameRegex.test(form.name)) {
+			errorSwal("Please enter a valid name.");
+			return;
+		}
 
-    if (!questionRegex.test(form.question)) {
-      errorSwal("Please enter a valid message.");
-      return;
-    } else return true;
-  };
+		if (!questionRegex.test(form.question)) {
+			errorSwal("Please enter a valid message.");
+			return;
+		} else return true;
+	};
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
-    setForm({ ...form, [name]: value });
-  };
+	const handleChange = (event) => {
+		const name = event.target.name;
+		const value = event.target.value;
+		setForm({ ...form, [name]: value });
+	};
 
-  const handlePhoneChange = (value) => {
-    setForm((prevForm) => ({
-      ...prevForm,
-      phone: value,
-    }));
-  };
+	const handlePhoneChange = (value) => {
+		setForm((prevForm) => ({
+			...prevForm,
+			phone: value,
+		}));
+	};
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!valueCheck(form)) return;
-    else {
-      setLoading(true);
-      try {
-        await emailjs.send(
-          import.meta.env.VITE_SERVICE,
-          import.meta.env.VITE_TEMPLATE,
-          {
-            name: form.name,
-            email: form.email,
-            phone: form.phone,
-            course: form.course,
-            question: form.question,
-          },
-          import.meta.env.VITE_KEY
-        );
-        successSwal();
-        setLoading(false);
-        setForm({ name: "", phone: "", email: "", course: "", question: "" });
-      } catch (error) {
-        console.log(error.text);
-        errorSwal("Your message hasn't been sent, please try again later.");
-        setLoading(false);
-      }
-    }
-  };
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (!valueCheck(form)) return;
+		else {
+			setLoading(true);
+			try {
+				await emailjs.send(
+					import.meta.env.VITE_SERVICE,
+					import.meta.env.VITE_TEMPLATE,
+					{
+						name: form.name,
+						email: form.email,
+						phone: form.phone,
+						course: form.course,
+						question: form.question,
+					},
+					import.meta.env.VITE_KEY
+				);
+				successSwal();
+				setLoading(false);
+				setForm({ name: "", phone: "", email: "", course: "", question: "" });
+			} catch (error) {
+				console.log(error.text);
+				errorSwal("Your message hasn't been sent, please try again later.");
+				setLoading(false);
+			}
+		}
+	};
 
-  return (
-    <div className="w-full grid grid-cols-3 self-center mt-16 pb-16 gap-8">
-      <div className="grid grid-rows-3 col-span-1 gap-4 self-center">
-        <img src={course1} alt="course_1" />
-        <img src={course2} alt="course_2" />
-        <img src={course3} alt="course_3" />
-      </div>
-      <form
-        className="col-span-2 p-8 rounded-xl shadow-xl bg-pinklight"
-        onSubmit={handleSubmit}
-      >
-        <h1 className="text-4xl text-center text-text font-[500] mb-8">
-          Contact Us
-        </h1>
-        <label className="flex flex-col text-text text-[1.2rem] font-[500] p-4">
-          *Your name:
-          <input
-            required
-            type="text"
-            value={form.name}
-            name="name"
-            placeholder="Enter your name"
-            onChange={handleChange}
-            className="text-text text-[1.2rem] font-[400] py-2 px-4 rounded-xl outline-none  outline-dotted outline-logopink"
-          />
-        </label>
-        <label className="flex flex-col text-text text-[1.2rem] font-[500] p-4">
-          *Your phone number:
-          <PhoneInput
-            required
-            country={"hu"}
-            value={form.phone}
-            onChange={handlePhoneChange}
-            inputStyle={phoneInputStyle}
-          />
-        </label>
-        <label className="flex flex-col text-text text-[1.2rem] font-[500] p-4">
-          *Your email address:
-          <input
-            required
-            type="email"
-            value={form.email}
-            name="email"
-            placeholder="Enter your email address"
-            onChange={handleChange}
-            className="text-text text-[1.2rem] font-[400] py-2 px-4 rounded-xl outline-none outline-dotted outline-logopink"
-          />
-        </label>
-        <label className="flex flex-col text-text text-[1.2rem] font-[500] p-4">
-          *Which course are you interested in:
-          <select
-            required
-            value={form.course}
-            name="course"
-            onChange={handleChange}
-            className="text-text text-[1.2rem] font-[400] py-2 px-4 rounded-xl outline-none outline-dotted outline-logopink"
-          >
-            <option value="" disabled hidden>
-              Choose from our courses
-            </option>
-            {courses.map((course) => (
-              <option key={course.id} value={course.title}>
-                {course.title}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label className="flex flex-col text-text text-[1.2rem] font-[500] p-4">
-          *Do you have any questions?
-          <textarea
-            required
-            value={form.question}
-            name="question"
-            onChange={handleChange}
-            rows={5}
-            className="text-text text-[1.2rem] font-[400] py-2 px-4 rounded-xl outline-none outline-dotted outline-logopink"
-          />
-        </label>
-        <button
-          className={`${
-            loading ? "cursor-progress" : "cursor-pointer"
-          } bg-logopink px-16 py-2 rounded-[15px] shadow-xl border-none hover:bg-pinkdark text-white text-[1rem] font-[500] mx-auto block`}
-          type="submit"
-          disabled={loading}
-        >
-          {loading ? "Sending..." : "Send"}
-        </button>
-      </form>
-    </div>
-  );
+	return (
+		<div className="w-full grid grid-cols-3 self-center mt-16 pb-16 px-24 gap-24 justify-center">
+			<div className="flex flex-col gap-4 self-center">
+				<img src={course1} alt="course_1" />
+				<img src={course2} alt="course_2" />
+				<img src={course3} alt="course_3" />
+			</div>
+			<form
+				className="col-span-2 p-8 rounded-xl shadow-xl bg-pinklight"
+				onSubmit={handleSubmit}
+			>
+				<h1 className="text-4xl text-center text-text font-[500] mb-8">
+					Contact Us
+				</h1>
+				<label className="flex flex-col text-text text-[1.2rem] font-[500] p-4">
+					*Your name:
+					<input
+						required
+						type="text"
+						value={form.name}
+						name="name"
+						placeholder="Enter your name"
+						onChange={handleChange}
+						className="text-text text-[1.2rem] font-[400] py-2 px-4 rounded-xl outline-none  outline-dotted outline-logopink"
+					/>
+				</label>
+				<label className="flex flex-col text-text text-[1.2rem] font-[500] p-4">
+					*Your phone number:
+					<PhoneInput
+						required
+						country={"hu"}
+						value={form.phone}
+						onChange={handlePhoneChange}
+						inputStyle={phoneInputStyle}
+					/>
+				</label>
+				<label className="flex flex-col text-text text-[1.2rem] font-[500] p-4">
+					*Your email address:
+					<input
+						required
+						type="email"
+						value={form.email}
+						name="email"
+						placeholder="Enter your email address"
+						onChange={handleChange}
+						className="text-text text-[1.2rem] font-[400] py-2 px-4 rounded-xl outline-none outline-dotted outline-logopink"
+					/>
+				</label>
+				<label className="flex flex-col text-text text-[1.2rem] font-[500] p-4">
+					*Which course are you interested in:
+					<select
+						required
+						value={form.course}
+						name="course"
+						onChange={handleChange}
+						className="text-text text-[1.2rem] font-[400] py-2 px-4 rounded-xl outline-none outline-dotted outline-logopink"
+					>
+						<option value="" disabled hidden>
+							Choose from our courses
+						</option>
+						{courses.map((course) => (
+							<option key={course.id} value={course.title}>
+								{course.title}
+							</option>
+						))}
+					</select>
+				</label>
+				<label className="flex flex-col text-text text-[1.2rem] font-[500] p-4">
+					*Do you have any questions?
+					<textarea
+						required
+						value={form.question}
+						name="question"
+						onChange={handleChange}
+						rows={5}
+						className="text-text text-[1.2rem] font-[400] py-2 px-4 rounded-xl outline-none outline-dotted outline-logopink"
+					/>
+				</label>
+				<button
+					className={`${
+						loading ? "cursor-progress" : "cursor-pointer"
+					} bg-logopink px-16 py-2 rounded-[15px] shadow-xl border-none hover:bg-pinkdark text-white text-[1rem] font-[500] mx-auto block`}
+					type="submit"
+					disabled={loading}
+				>
+					{loading ? "Sending..." : "Send"}
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default CourseForm;
--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -4,88 +4,96 @@ import { courses } from "../constants";
 import { CourseForm } from "../components";
 
 const CoursePage = () => {
-  const { id } = useParams();
-  let course = courses.filter((course) => course.id === id)[0];
+	const { id } = useParams();
+	let course = courses.filter((course) => course.id === id)[0];
 
-  const [imagesLoaded, setImagesLoaded] = useState(false);
-  const [images, setImages] = useState([]);
+	const [imagesLoaded, setImagesLoaded] = useState(false);
+	const [images, setImages] = useState([]);
 
-  const loadImage = async (imageName) => {
-    const { default: image } = await import(`../assets/${imageName}.webp`);
-    return image;
-  };
+	const loadImage = async (imageName) => {
+		const { default: image } = await import(`../assets/${imageName}.webp`);
+		return image;
+	};
 
-  useEffect(() => {
-    const loadImages = async () => {
-      const imagePromises = course.imageArray.map((imageName) =>
-        loadImage(imageName)
-      );
-      const loadedImages = await Promise.all(imagePromises);
-      setImages(loadedImages);
-      setImagesLoaded(true);
-    };
+	useEffect(() => {
+		const loadImages = async () => {
+			const imagePromises = course.imageArray.map((imageName) =>
+				loadImage(imageName)
+			);
+			const loadedImages = await Promise.all(imagePromises);
+			setImages(loadedImages);
+			setImagesLoaded(true);
+		};
 
-    loadImages();
-  }, []);
+		loadImages();
+	}, []);
 
-  if (!imagesLoaded) {
-    return null;
-  }
+	if (!imagesLoaded) {
+		return null;
+	}
 
-  return (
-    <div className="mt-56 w-[85%] flex flex-col items-center">
-      <div className="w-[85% bg-white rounded-xl p-8 flex flex-col">
-        <h1 className="text-4xl text-center text-text font-[500] mb-8">
-          {course.title}
-        </h1>
-        {course.longDetails &&
-          course.longDetails.map((detail, index) => {
-            if (Array.isArray(detail)) {
-              return (
-                <div
-                  key={index}
-                  className="flex items-center justify-center gap-16 mb-8"
-                >
-                  <span className="flex flex-col gap-16">
-                    <img src={images[index]} alt="course-picture" />
-                    <img src={images[index + 2]} alt="course-picture" />
-                  </span>
-                  <ul className="list-disc pl-6 mb-8 w-[60rem]">
-                    {detail.map((item, itemIndex) => (
-                      <li
-                        key={itemIndex}
-                        className={`text-text text-lg font-[400] text-justify ${
-                          itemIndex === 0 ? "font-[600] list-none" : ""
-                        }`}
-                      >
-                        {item}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              );
-            } else {
-              return (
-                <div
-                  key={index}
-                  className="flex items-center justify-center gap-16"
-                >
-                  <p className="text-text text-lg text-justify font-[400] mb-8">
-                    {detail}
-                  </p>
-                  <img
-                    src={images[index]}
-                    alt="course-picture"
-                    className="w-[20rem] mb-8"
-                  />
-                </div>
-              );
-            }
-          })}
-        <CourseForm courses={courses} />
-      </div>
-    </div>
-  );
+	return (
+		<div className="mt-56 w-[85%] flex flex-col items-center">
+			<div className="w-[85% bg-white rounded-xl p-12 flex flex-col">
+				<h1 className="text-4xl text-center text-text font-[500] mb-8">
+					{course.title}
+				</h1>
+				{course.longDetails &&
+					course.longDetails.map((detail, index) => {
+						if (Array.isArray(detail)) {
+							return (
+								<div
+									key={index}
+									className="flex items-center justify-center gap-16 mb-8"
+								>
+									<span className="flex flex-col gap-16">
+										<img
+											src={images[index]}
+											alt="course-picture"
+											className="w-[30rem]"
+										/>
+										<img
+											src={images[index + 2]}
+											alt="course-picture"
+											className="w-[30rem]"
+										/>
+									</span>
+									<ul className="list-disc pl-6 mb-8 w-[60rem]">
+										{detail.map((item, itemIndex) => (
+											<li
+												key={itemIndex}
+												className={`text-text text-lg font-[400] text-justify ${
+													itemIndex === 0 ? "font-[600] list-none" : ""
+												}`}
+											>
+												{item}
+											</li>
+										))}
+									</ul>
+								</div>
+							);
+						} else {
+							return (
+								<div
+									key={index}
+									className="flex items-center justify-center gap-16"
+								>
+									<p className="text-text text-lg text-justify font-[400] mb-8">
+										{detail}
+									</p>
+									<img
+										src={images[index]}
+										alt="course-picture"
+										className="w-[30rem] mb-8"
+									/>
+								</div>
+							);
+						}
+					})}
+				<CourseForm courses={courses} />
+			</div>
+		</div>
+	);
 };
 
 export default CoursePage;
