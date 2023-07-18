7c8092db1297fd80e07c2f45c2044d71543d98a8
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-07-18 17:00:22 +0300

    modified styles for nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
 	return (
 		<footer className="w-full flex flex-col mt-24 bg-pinklight text-text">
 			<img src={jam} alt="jam" className="h-[7rem]" style={recolorStyle} />
-			<div className="flex flex-wrap justify-between items-center px-20">
+			<div className="flex flex-wrap sm:flex-row flex-col justify-between items-center sm:px-20">
 				<div className="flex flex-col items-center justify-center">
 					<div
 						alt="logo"
@@ -19,15 +19,18 @@ const Footer = () => {
 						2023 Le Ciel Sucré&copy; <br /> All rights reserved.
 					</p>
 				</div>
-				<div className="flex w-[50%] justify-between">
+				<div className="flex sm:flex-row flex-col sm:pl-0 pl-8 sm:pt-0 pt-8 sm:w-[50%] w-full justify-between">
 					{footerLinks.map((link) => (
-						<div key={link.title} className="flex flex-col leading-8">
-							<p className="font-[600] text-[1.2rem] text-logopink">
+						<div key={link.title} className="flex flex-col leading-8 pb-8">
+							<p className="font-[600] sm:text-[1.2rem] text-[1.5rem] text-logopink">
 								{link.title}
 							</p>
 							<ul>
 								{link.links.map((item) => (
-									<li key={item.title} className="font-[300] text-[1rem]">
+									<li
+										key={item.title}
+										className="font-[300] sm:text-[1rem] text-[1.2rem]"
+									>
 										<Link to={item.url} className="hover:text-logopink">
 											{item.title}
 										</Link>
