ecdf5e7fe922279a017a78a397cab122cab255fc
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-07-18 20:36:19 +0300

    changed style

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -4,7 +4,7 @@ import { courses } from "../constants";
 const Courses = () => {
 	return (
 		<div className="md:mt-56 mt-36 w-full flex flex-col items-center">
-			<div className="md:bg-white rounded-xl md:p-8 flex flex-col items-center">
+			<div className="md:bg-white rounded-xl md:p-16 flex flex-col items-center">
 				<h1 className="text-brown xl:text-[3rem] text-[2rem] font-[400] mb-8">Our Courses</h1>
 				<div className="w-full flex md:flex-row flex-col justify-between gap-4 md:flex-wrap items-center justify-center">
 					{courses.map((course, index) => (
