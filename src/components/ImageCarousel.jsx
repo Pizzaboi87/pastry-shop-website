a53b863031f615c52b6b93a7a8225a349565263b
Pizzaboi87 <112249710+Pizzaboi87@users.noreply.github.com>
2023-07-19 20:03:19 +0300

    added responsiveness to postcard

--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -4,12 +4,17 @@ import "react-multi-carousel/lib/styles.css";
 const ImageCarousel = ({ children }) => {
 	const responsive = {
 		desktop: {
-			breakpoint: { max: 3000, min: 1024 },
+			breakpoint: { max: 3000, min: 1401 },
 			items: 4,
 			slidesToSlide: 1,
 		},
+		medium: {
+			breakpoint: { max: 1400, min: 1024 },
+			items: 3,
+			slidesToSlide: 1,
+		},
 		tablet: {
-			breakpoint: { max: 1024, min: 768 },
+			breakpoint: { max: 1023, min: 768 },
 			items: 2,
 			slidesToSlide: 1,
 		},
@@ -31,7 +36,8 @@ const ImageCarousel = ({ children }) => {
 			showDots={true}
 			infinite={true}
 			partialVisible={false}
-			dotListClass="custom-dot-list-style"
+			renderDotsOutside={true}
+			dotListClass="custom-dot-list-style mb-3"
 		>
 			{children}
 		</Carousel>
